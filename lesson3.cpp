4a7b8e958640c125c7ba84de149607404293b200
tanphuocgialai606-lab <tanphuocgialai606@gmail.com>
2025-09-21 23:11:39 +0700

    Add files via upload

---
+++ lesson3.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+    long long n, count =0;
+    scanf("%lld",&n);
+    long long a= (long long) sqrt(n);
+    for(int i=1; i<=a;i++){
+        if (n%i==0){
+            if(i==n/i){
+                count++;
+            }
+            else{
+                count+=2;
+            }
+        }
+    }
+    printf("%lld",count);
+    return 0;
+}
